4b45657d10a4b5c63352719a514b59d17adb037b
Ivan Nikolskiy <enty8080@gmail.com>
2024-10-14 13:15:11 +0100

    Update templatemo-script.js

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -53,5 +53,5 @@ $(document).ready(function () {
         setBgOverlay();
     });
 
-    $(document).getElementById("year").innerHTML = new Date().getFullYear();
+    document.getElementById("year").innerHTML = new Date().getFullYear();
 });
